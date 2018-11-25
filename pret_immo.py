f7111c4b276d7c2f62f3bcb00dbe39b90bc9ed13
lloisel88 <39280549+lloisel88@users.noreply.github.com>
2018-11-25 17:54:53 +0000

    nouvelle formule qui démontre que le calcul initial était mauvais

--- pret_immo.py
+++ pret_immo.py
@@ -10,16 +10,39 @@ def calcul_interets(capital = 500000, taux = 0.015, annees = 20):
 # print(calcul_interets(500000, 0.015, 20))
 
 # plot l'augmentation des intérêts pour un prêt donné en fonction des années 
-ann = range(15,26)
-interets_annees = [calcul_interets(annees = i) 
-                    for i in ann]
+# ann = range(15,26)
+# interets_annees = [calcul_interets(annees = i) 
+#                     for i in ann]
 
-plt.plot(ann, interets_annees, marker = "o", linestyle = "solid")
-plt.xlabel("Années de remboursement")
-plt.ylabel("Montant des intérêts totaux")
-plt.title(r"Montant des intérêts pour un prêt de 500 000 € avec un taux de 1,5% en fonction du nombre d'années")
-plt.show()
+# plt.plot(ann, interets_annees, marker = "o", linestyle = "solid")
+# plt.xlabel("Années de remboursement")
+# plt.ylabel("Montant des intérêts totaux")
+# plt.title(r"Montant des intérêts pour un prêt de 500 000 € avec un taux de 1,5% en fonction du nombre d'années")
+# plt.show()
 
-# plot le remboursement du prêt 
-# print(calcul_interets(annees = annee) for annee in annees)
+# re-calcul des intérêts totaux en prenant pour point de départ l'annuité basée sur les intérêts totaux calculés
+# on veut voir si on retombe sur nos pattes (est-ce que l'hypothèse de la décroissance linéaire des intérêts à rembourser est valide)
+def calcul_interets_methode_2(capital = 500000, taux = 0.015, annees = 20):
+    # initialisation des listes utilisées
+    liste_interets_annuel = []
+    liste_capital_annuel = []
+    liste_capital_restant = []
+    # remboursement total = capital + intérêts totaux 
+    remboursement_total = capital = calcul_interets(capital, taux, annees) 
+    annuite = remboursement_total / annees
+    # au début il nous reste tout le capital à rembourser
+    capital_restant = capital 
+    # pour chaque annee, on calcule les intérêts à rembourser, basés sur le capital restant, puis le capital remboursé, enfin on met à jour le capital restant
+    for _ in range(annees):
+        interets_annuel = capital_restant * taux
+        liste_interets_annuel.append(interets_annuel)
+        capital_annuel = annuite - interets_annuel
+        liste_capital_annuel.append(capital_annuel)
+        capital_restant = capital_restant - capital_annuel 
+        liste_capital_restant.append(capital_restant)
+    return sum(liste_interets_annuel)
+
+print(calcul_interets(), calcul_interets_methode_2())
+
+
 
